e64914fa8c6f03f5affc9f5fc57af0310c0a9089
jose <jose.fc182@gmail.com>
2020-07-20 10:32:25 -0500

    JLF: LOGIN v1

--- app/src/main/java/com/uisrael/proyecto_final/login.java
+++ app/src/main/java/com/uisrael/proyecto_final/login.java
@@ -2,13 +2,34 @@ package com.uisrael.proyecto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
+    Button enviar;
+    TextView usuario,clave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        enviar=findViewById(R.id.btnEnviar);
+        usuario=findViewById(R.id.txtso);
+        clave=findViewById(R.id.txtPasswd);
+    }
+    public void verificar (View v){
+
+        if(usuario.getText().toString().equals("estudiante2020") && clave.getText().toString().equals("uisrael2020")){
+            Intent intentEnvio = new Intent( this, ingresoVehiculo.class);
+            intentEnvio.putExtra("nombreUsuario",usuario.getText().toString());
+            startActivity(intentEnvio);
+        }else{
+            Toast.makeText(getApplicationContext(),"Usuario o Clave Incorrecto.",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
