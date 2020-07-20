5ca138ec59158d256c6c85edc43a32f20632ae6a
diegoatjim <JnPxxKfNBdL4u3C>
2020-07-20 09:19:42 -0500

    Cambio validacion

--- app/src/main/java/com/uisrael/proyecto_final/Clientes.java
+++ app/src/main/java/com/uisrael/proyecto_final/Clientes.java
@@ -68,71 +68,62 @@ public class Clientes extends AppCompatActivity {
             String inputLine;
             StringBuffer response = new StringBuffer();
             String json="";
-
-            while ((inputLine = in.readLine())!=null){
-                response.append(inputLine);
-            }
-
-            json = response.toString();
-            // para consulta individual
-           if(!nCodigo.isEmpty()){
-               json = "["+json+"]";
-           }
-
-            JSONArray jsonArr = new JSONArray(json);
-
-            String nombre = "";
-            String direccion = "";
-            String telefono = "";
-            String email = "";
-            String identificacion = "";
-            String codigo="";
-            String tipo ="";
-
-            for (int i = 0; i<jsonArr.length();i++){
-                JSONObject objeto = jsonArr.getJSONObject(i);
-                codigo = objeto.optString("cli_id");
-                nombre = objeto.optString("cli_nombre");
-                identificacion = objeto.optString("cli_identificacion");
-                telefono= objeto.optString("cli_telefono");
-                email= objeto.optString("cli_email");
-                direccion= objeto.optString("cli_direccion");
-                tipo= objeto.optString("cli_tipo_id");
-
-
-                txtCodigo.setText(codigo);
-                txtNombre.setText(nombre);
-                txtIdent.setText(identificacion);
-                txtDireccion.setText(direccion);
-                txtTelefono.setText(telefono);
-                txtEmail.setText(email);
-
-                if( tipo =="Cedula"){
-                    //opCedula.setChecked(true);
-                    ((RadioButton) rgGrupo.getChildAt(0)).setChecked(true);
-                    ((RadioButton) rgGrupo.getChildAt(1)).setChecked(false);
-                    ((RadioButton) rgGrupo.getChildAt(2)).setChecked(false);
-
-                }
-                if(tipo =="Ruc"){
-                    //opRuc.setChecked(true);
-                    ((RadioButton) rgGrupo.getChildAt(1)).setChecked(true);
-                    ((RadioButton) rgGrupo.getChildAt(0)).setChecked(false);
-                    ((RadioButton) rgGrupo.getChildAt(2)).setChecked(false);
-
-
-                }
-                if(tipo =="Pasaporte"){
-                    //opPasaporte.setChecked(true);
-                    ((RadioButton) rgGrupo.getChildAt(2)).setChecked(true);
-                    ((RadioButton) rgGrupo.getChildAt(0)).setChecked(false);
-                    ((RadioButton) rgGrupo.getChildAt(1)).setChecked(false);
-                }
-
+            if(response.toString().isEmpty()){
+                Toast.makeText(this,"No Tiene Datos",Toast.LENGTH_LONG).show();
+                LimpiaForm();
 
             }
+            else {
+
+                    while ((inputLine = in.readLine())!=null){
+                        response.append(inputLine);
+                         }
+
+                    json = response.toString();
+
+                    JSONArray jsonArr = new JSONArray(json);
+
+                    String nombre = "";
+                    String direccion = "";
+                    String telefono = "";
+                    String email = "";
+                    String identificacion = "";
+                    String codigo="";
+                    String tipo ="";
+
+                    for (int i = 0; i<jsonArr.length();i++){
+                        JSONObject objeto = jsonArr.getJSONObject(i);
+                        codigo = objeto.optString("cli_id");
+                        nombre = objeto.optString("cli_nombre");
+                        identificacion = objeto.optString("cli_identificacion");
+                        telefono= objeto.optString("cli_telefono");
+                        email= objeto.optString("cli_email");
+                        direccion= objeto.optString("cli_direccion");
+                        tipo= objeto.optString("cli_tipo_id");
 
 
+                        txtCodigo.setText(codigo);
+                        txtNombre.setText(nombre);
+                        txtIdent.setText(identificacion);
+                        txtDireccion.setText(direccion);
+                        txtTelefono.setText(telefono);
+                        txtEmail.setText(email);
+
+                        if( tipo.equals("Cedula") ){
+                            opCedula.setChecked(true);
+
+                        }
+                        if(tipo.equals("Ruc")){
+                            opRuc.setChecked(true);
+
+                        }
+                        if(tipo.equals("Pasaporte")){
+                            opPasaporte.setChecked(true);
+
+                            }
+                    }
+
+            }
 
         }
         catch (MalformedURLException e) {
@@ -166,12 +157,14 @@ public class Clientes extends AppCompatActivity {
         }
         postCli servicioTask= new postCli(this,"http://192.168.100.9/garajeuio/postcli.php",nombre,identificacion,tipo, direccion,telefono,email);
         servicioTask.execute();
+        Toast.makeText(this,"Registro Grabado",Toast.LENGTH_LONG);
         LimpiaForm();
     }
     public void deleteCli(View v){
         int codigo = Integer.parseInt( txtCodigo.getText().toString());//Clic GRID //
         deleteCli servicioTask= new deleteCli(this,"http://192.168.100.9/garajeuio/postcli.php?codigo="+codigo);
         servicioTask.execute();
+        Toast.makeText(this,"Registro Eliminado",Toast.LENGTH_LONG);
         LimpiaForm();
     }
     public void actualizaCli(View v){
@@ -196,6 +189,7 @@ public class Clientes extends AppCompatActivity {
         String sCript ="cli_id="+codigo+"&cli_nombre="+nombre+"&cli_tipo_id="+tipo+"&cli_identificacion="+identificacion+"&cli_email="+email+"&cli_telefono="+telefono+"&cli_direccion="+direccion;
         putCli servicioTask= new putCli(this,"http://192.168.100.9/garajeuio/postcli.php?"+sCript);
         servicioTask.execute();
+        Toast.makeText(this,"Registro Actualizado",Toast.LENGTH_LONG);
         LimpiaForm();
 
     }
